f05d9f84e31e0488b4e1c12bb24ed916e60055c9
jorgeduje <jorgeduje4@gmail.com>
2024-11-14 20:56:22 -0300

    se agregan productos

---
+++ src/products.js
@@ -0,0 +1,11 @@
+const products = [
+  {
+    title: "zapatila nike",
+    price: 12432,
+    description: "estas son las mejores",
+    imageUrl:
+      "https://res.cloudinary.com/dnqfh2chg/image/upload/v1686581567/images_teykwq.jpg",
+    stock: 4,
+    category: "deportivas",
+  },
+];
