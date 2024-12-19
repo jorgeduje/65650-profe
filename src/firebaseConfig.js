1f2f5ab2bdcbb055312f25be604479ecde7fb887
jorgeduje <jorgeduje4@gmail.com>
2024-12-19 19:26:04 -0300

    se agrega envs

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpJplNx-06C6lrlvVo6Hc8wDW4WagwXwg",
-  authDomain: "comsion-65650.firebaseapp.com",
-  projectId: "comsion-65650",
-  storageBucket: "comsion-65650.firebasestorage.app",
-  messagingSenderId: "968761514118",
-  appId: "1:968761514118:web:fceb9f1746f43c374fd907",
+  apiKey: import.meta.env.VITE_KEY,
+  authDomain: import.meta.env.VITE_AUTH,
+  projectId: import.meta.env.VITE_PROJECT,
+  storageBucket: import.meta.env.VITE_STORAGE,
+  messagingSenderId: import.meta.env.VITE_MESSAGING,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
